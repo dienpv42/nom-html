dcbbabe31ba44402c4ae5a236a10f9d666d0a519
Dien Pham Van <Dienpv42@gmail.com>
2021-07-25 02:27:45 +0700

    f

---
+++ assets/js/app.js
@@ -0,0 +1,16 @@
+
+
+(function ($) {
+    "use strict";
+  
+    $( ".section_banner" ).each(function() {
+        var attr = $(this).attr('data-image-src');
+      
+        if (typeof attr !== typeof undefined && attr !== false) {
+            $(this).css('background', 'url('+attr+')');
+        }
+      
+    });
+  
+})(window.jQuery);
+  
