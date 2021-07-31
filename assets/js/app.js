72c9a529f52fee36443b3cf9dd70c5c98b6ddc28
Dien Pham Van <Dienpv42@gmail.com>
2021-08-01 02:08:10 +0700

    up

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,9 +2,18 @@
 
 (function ($) {
     "use strict";
+
+    var $window = $(window),
+    $body = $("body");
   
     $( ".section_banner" ).each(function() {
         var attr = $(this).attr('data-image-src');
+        if (typeof attr !== typeof undefined && attr !== false) {
+            $(this).css("background-image", "url("+attr+")");
+        }
+    });
+    $( ".section_about_banner_ft" ).each(function() {
+      var attr = $(this).attr('data-image-src');
         if (typeof attr !== typeof undefined && attr !== false) {
             $(this).css("background-image", "url("+attr+")");
         }
@@ -60,9 +69,69 @@
             },
         });
 
-
     });
 
+    /*---------------------------------
+            Off Canvas toggler Function
+        -----------------------------------*/
+
+  var $offCanvasToggle = $(".offcanvas-toggle, .offcanvas-overlay"),
+        $offCanvas = $(".offcanvas"),
+        $offCanvasOverlay = $(".offcanvas-overlay"),
+        $mobileMenuToggle = $(".mobile-menu-toggle");
+        $offCanvasToggle.on("click", function (e) {
+            e.preventDefault();
+            $body.toggleClass("offcanvas-open");
+            $offCanvasOverlay.fadeToggle();
+            $offCanvas.fadeToggle();
+            $(".offcanvas-toggle").toggleClass("close");
+        });
+
+        // $(".offcanvas-toggle.close, .offcanvas-overlay").on("click", function (e) {
+        // e.preventDefault();
+        //     $body.removeClass("offcanvas-open");
+        //     $offCanvas.removeClass("offcanvas-open");
+        //     $offCanvasOverlay.fadeOut();
+        //     $mobileMenuToggle.find("a").removeClass("close");
+        // });
+        /*----------------------------------
+             Off Canvas Menu
+         -----------------------------------*/
+
+  function mobileOffCanvasMenu() {
+    var $offCanvasNav = $(".offcanvas-menu, .overlay-menu"),
+      $offCanvasNavSubMenu = $offCanvasNav.find(".offcanvas-submenu");
+    /*Add Toggle Button With Off Canvas Sub Menu*/
+
+    $offCanvasNavSubMenu.parent().prepend('<span class="menu-expand"></span>');
+    /*Category Sub Menu Toggle*/
+
+    $offCanvasNav.on("click", "li a, .menu-expand", function (e) {
+      var $this = $(this);
+
+      if ($this.attr("href") === "#" || $this.hasClass("menu-expand")) {
+        e.preventDefault();
+
+        if ($this.siblings("ul:visible").length) {
+          $this.parent("li").removeClass("active");
+          $this.siblings("ul").slideUp();
+          $this.parent("li").find("li").removeClass("active");
+          $this.parent("li").find("ul:visible").slideUp();
+        } else {
+          $this.parent("li").addClass("active");
+          $this
+            .closest("li")
+            .siblings("li")
+            .removeClass("active")
+            .find("li")
+            .removeClass("active");
+          $this.closest("li").siblings("li").find("ul:visible").slideUp();
+          $this.siblings("ul").slideDown();
+        }
+      }
+    });
+  }
+  mobileOffCanvasMenu()
 
     /* ==========================================================================
     Scroll menu
